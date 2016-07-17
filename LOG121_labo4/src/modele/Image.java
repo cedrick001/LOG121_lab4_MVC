55109e6205d6e129b0bd183a7e06eb42fa783ff1
cedrick001 <cedrick.pipitone.1@ens.etsmtl.ca>
2016-07-17 16:04:38 -0400

    Remplissage Image.java

--- LOG121_labo4/src/modele/Image.java
+++ LOG121_labo4/src/modele/Image.java
@@ -1,5 +1,56 @@
+
+
 package modele;
 
-public class Image {
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import java.io.Serializable;
+
+
+
+
+public class Image implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+	private BufferedImage image;
+	
+	
+/*****************
+* 	
+* Constructeur par copie d'attribut
+* 	 
+********/
+	public Image (BufferedImage image){
+		this.image = image;
+		}
+
+	
+	
+	
+/*****************
+*
+*	Setters/getters
+* 
+***************/
+public BufferedImage getImage() {
+	return image;
+}
+
+public void setImage(BufferedImage image) {
+	this.image = image;
+}
+/*****************
+*
+*	Méthodes
+* 
+***************/
+
+public void importImage(BufferedImage image) {
+	this.image = image;
+}
+
+public void sauvegarderImage(){
+
+}
 
 }
---
+++ LOG121_labo4/src/modele/Test.java
@@ -0,0 +1,31 @@
+package modele;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.awt.image.BufferedImage;
+
+public class Test {
+
+	public static void main(String[] args) throws FileNotFoundException, IOException {
+		
+		FileOutputStream fileOut =
+		         new FileOutputStream("image.serial");
+		
+		
+		ObjectOutputStream oos =  new ObjectOutputStream(fileOut) ;
+		
+		
+		// Création de l'objet
+		BufferedImage test = new BufferedImage(50,50,BufferedImage.TYPE_INT_BGR);
+		Image image = new Image(test);
+		oos.writeObject(image);
+		oos.flush();
+		oos.close();
+		fileOut.close();
+        
+	}
+
+}
