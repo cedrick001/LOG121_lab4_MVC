4bc1d92ec745ddaffdf4f137e32fea2fde84eeb0
cedrick001 <cedrick.pipitone.1@ens.etsmtl.ca>
2016-07-20 12:05:49 -0400

    Ajout des methodes sauvegarde() et import()

--- LOG121_labo4/src/modele/Image.java
+++ LOG121_labo4/src/modele/Image.java
@@ -4,24 +4,36 @@ package modele;
 
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.io.Serializable;
+import java.util.Scanner;
+
+import javax.imageio.ImageIO;
 
 
 
 
 public class Image implements Serializable {
 
-	private static final long serialVersionUID = 1L;
-	private BufferedImage image;
+	
+	private static final long serialVersionUID = -984752116648850748L;
+	private transient BufferedImage image;
+	private int coordonneeX = 0;
+	private int coordonneeY = 0;
 	
 	
 /*****************
 * 	
 * Constructeur par copie d'attribut
+ * @throws IOException 
 * 	 
 ********/
-	public Image (BufferedImage image){
-		this.image = image;
+	public Image (File image) throws IOException{
+		setImage(ImageIO.read(image));
 		}
 
 	
@@ -42,14 +54,38 @@ public void setImage(BufferedImage image) {
 /*****************
 *
 *	Méthodes
+ * @throws IOException 
 * 
 ***************/
 
-public void importImage(BufferedImage image) {
-	this.image = image;
+public void importImage(String image) throws IOException {
+	
+	setImage(ImageIO.read((new File(image))));
 }
 
-public void sauvegarderImage(){
+
+
+
+public void sauvegarderImage() throws IOException{
+	
+	
+	Scanner sc = new Scanner(System.in);
+	System.out.println("Veuillez saisir le nom du fichier de destination :");
+	String fileName = sc.nextLine();
+	
+	
+	//Création du répertoire de destination et du flux de sortie
+	FileOutputStream fileOut = new FileOutputStream("d:"+ fileName + ".serial");
+	ObjectOutputStream oos =  new ObjectOutputStream(fileOut) ;
+	
+	
+
+	
+	//Ecriture dans un fichier
+	oos.writeObject(this);
+	oos.flush();
+	oos.close();
+	fileOut.close();
 
 }
 
--- LOG121_labo4/src/modele/Perspective.java
+++ LOG121_labo4/src/modele/Perspective.java
@@ -1,5 +1,27 @@
 package modele;
 
-public class Perspective {
+import java.awt.image.BufferedImage;
+import java.io.Serializable;
 
+public class Perspective implements Serializable{
+		
+	private static final long serialVersionUID = 469917637474822L;
+	private int valeurZoom;
+		
+		
+		
+		
+		
+		
+public void zoomIn() {
+}
+
+public void zoomOut(){
+	
+}
+
+public int calculZoom(int zoom){
+	int valeurZoom = 0;
+	return valeurZoom;
+}
 }
